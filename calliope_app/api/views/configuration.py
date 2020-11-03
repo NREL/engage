58cb3d7a1ab99c6b2020fd9cbcd4aae37c2c42b3
jgu2 <jianli.gu@nrel.gov>
2020-11-02 23:38:57 -0700

    Refactor wtk_timeseries download

--- calliope_app/api/views/configuration.py
+++ calliope_app/api/views/configuration.py
@@ -1,8 +1,16 @@
+import functools
+import json
+import os
+
+import numpy as np
+from django.conf import settings
 from django.views.decorators.csrf import csrf_protect
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from PySAM import Windpower
+from PySAM.ResourceTools import FetchResourceFiles
 
 from api.models.calliope import Abstract_Tech, Run_Parameter
 from api.models.configuration import Location, Technology, Tech_Param, \
@@ -11,17 +19,6 @@ from api.models.configuration import Location, Technology, Tech_Param, \
     Model_Favorite, User_File, Model_User
 from api.tasks import task_status, upload_ts
 from taskmeta.models import CeleryTask
-
-import json
-import numpy as np
-from PySAM import Windpower
-from PySAM.ResourceTools import FetchResourceFiles
-
-NREL_API_EMAIL = 'engage@nrel.gov'
-NREL_API_KEY = 'jh3elray8wvgxaLMmneG2nIDO85XzeUNKJEHXLmK'
-
-
-# ------ Models ------
 
 
 @csrf_protect
@@ -1221,22 +1218,12 @@ def wtk_timeseries(request):
     Example:
     POST: /api/wtk_timeseries/
     """
-
     latitude = request.POST["lat"]
     longitude = request.POST["lon"]
-    COORDINATES = (longitude, latitude)
+    coordinate = (longitude, latitude)
 
     # Fetch wind resource data
-    wr = FetchResourceFiles(tech='wind',
-                            resource_year='tmy',
-                            nrel_api_email=NREL_API_EMAIL,
-                            nrel_api_key=NREL_API_KEY,
-                            resource_dir='wind_data')
-    wr.fetch([COORDINATES])
-
-    # --- Get resource data file path ---
-    wtk_path_dict = wr.resource_file_paths_dict
-    wtk_fp = wtk_path_dict[COORDINATES]
+    wtk_fp = wtk_fetch_resource_files(coordinate)
 
     # --- Initialize generator ---
     if wtk_fp is not None:
@@ -1250,3 +1237,21 @@ def wtk_timeseries(request):
         payload = {"message": "Not Found"}
 
     return HttpResponse(json.dumps(payload), content_type="application/json")
+
+
+def wtk_fetch_resource_files(coordinate):
+    """Fetch wind resource data"""
+    wr = FetchResourceFiles(
+        tech='wind',
+        resource_year='tmy',
+        nrel_api_email=settings.NREL_API_EMAIL,
+        nrel_api_key=settings.NREL_API_KEY,
+        resource_dir=os.path.join(settings.DATA_STORAGE, 'wind-data')
+    )
+    wr.fetch([coordinate])
+
+    # --- Get resource data file path ---
+    wtk_path_dict = wr.resource_file_paths_dict
+    wtk_fp = wtk_path_dict[coordinate]
+
+    return wtk_fp
