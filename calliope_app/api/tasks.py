02d0285030eb113bd9edc2db007690d4d407a66f
jgu2 <jianli.gu@nrel.gov>
2020-12-15 09:25:14 -0700

    Add region into s3 client

--- calliope_app/api/tasks.py
+++ calliope_app/api/tasks.py
@@ -442,6 +442,7 @@ class CalliopeModelRunTask(Task):
 
         client = boto3.client(
             service_name="s3",
+            region_name="us-west-2",
             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
         )
