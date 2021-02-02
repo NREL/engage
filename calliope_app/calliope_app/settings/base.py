76f39b4db1a90ddf005816692fb2267312d7a051
jgu2 <jianli.gu@nrel.gov>
2021-02-01 17:56:32 -0700

    Add debug toolbar

--- calliope_app/calliope_app/settings/base.py
+++ calliope_app/calliope_app/settings/base.py
@@ -38,6 +38,7 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar'
 ]
 
 LOCAL_APPS = [
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 # TEMPLATES
