6884fabfa9deeabb2afc6a836ac941a496a95e71
Rob Spencer <robert.spencer@nrel.gov>
2021-07-28 02:36:51 -0600

    debug: Float values are getting concatenated

--- calliope_app/client/templatetags/add_commas.py
+++ calliope_app/client/templatetags/add_commas.py
@@ -9,6 +9,8 @@ def add_commas(item):
         dec = str(item).split('.')
         if len(dec) > 1:
             length = len(dec[1])
+        elif 'e-' in item:
+            length = item.split('e-')[1]
         frmt = "{:,." + str(length) + "f}"
         pretty_string = frmt.format(float(item))
         return pretty_string
