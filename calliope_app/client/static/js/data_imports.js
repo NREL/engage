238da28a4fdc7b54b7416ddc00ca364b4edadd8f
hfields <hanna.fields@nrel.gov>
2023-02-01 11:56:37 -0700

    Start group constraint impl, need to add dropdown and list concept to Constraints

--- calliope_app/client/static/js/data_imports.js
+++ calliope_app/client/static/js/data_imports.js
@@ -55,6 +55,7 @@ function activate_import_btns() {
 	$('.pvwatts').on('click', function() {
 		$('#pvwatts_form').show();
 		$('#wtk_form').hide();
+        $('#scenario_settings_json_form').hide();
 		$("#data-source-modal").css('display', "block");
 		var location_id = +$(this).parents('tr').attr('data-location_id'),
 			meta = get_loc_meta(location_id);
@@ -64,8 +65,9 @@ function activate_import_btns() {
 	$('.wtk:visible').attr('disabled', false);
 	$('.wtk').unbind();
 	$('.wtk').on('click', function() {
+        $('#wtk_form').show();
 		$('#pvwatts_form').hide();
-		$('#wtk_form').show();
+        $('#scenario_settings_json_form').hide();
 		$("#data-source-modal").css('display', "block");
 		var location_id = +$(this).parents('tr').attr('data-location_id'),
 			meta = get_loc_meta(location_id);
