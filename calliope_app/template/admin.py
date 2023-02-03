18d588194f31a11c9191b8e3d2c24f606e99296e
hfields <hanna.fields@nrel.gov>
2023-02-03 11:50:15 -0700

    Import tables to admin.py

--- calliope_app/template/admin.py
+++ calliope_app/template/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from template.models import Template, Template_Option_Param, Template_Loc_Structure, Template_Tech_Structure, Template_Loc_Tech_Structure, Template_Parameter
 
 # Register your models here.
