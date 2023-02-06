d05956e3c3731a0e19a4293293231bc6e528e24e
hfields <hanna.fields@nrel.gov>
2023-02-06 12:47:39 -0700

    Fix class references within models.py

--- calliope_app/template/models.py
+++ calliope_app/template/models.py
@@ -25,7 +25,7 @@ class Template_Option_Param(models.Model):
     units = models.CharField(max_length=200)
     default_value = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    template = models.ForeignKey(template, on_delete=models.CASCADE)
+    template = models.ForeignKey(Template, on_delete=models.CASCADE)
 
     def __str__(self):
         return '%s' % (self.pretty_name)
@@ -38,7 +38,7 @@ class Template_Loc_Structure(models.Model):
     name = models.CharField(max_length=200)
     latitude_offset = models.FloatField()
     longitude_offset = models.FloatField()
-    template = models.ForeignKey(template, on_delete=models.CASCADE)
+    template = models.ForeignKey(Template, on_delete=models.CASCADE)
     primary_location = models.ForeignKey(Location,
                                 on_delete=models.CASCADE,
                                 related_name="primary_location",
@@ -58,7 +58,7 @@ class Template_Tech_Structure(models.Model):
     carrier_in = models.CharField(max_length=200)
     carrier_out = models.CharField(max_length=200)
     #color = models.CharField(max_length=200) looks like technologies is doing something fancy with this
-    template = models.ForeignKey(template, on_delete=models.CASCADE)
+    template = models.ForeignKey(Template, on_delete=models.CASCADE)
 
     # @property
     # def carrier_in(self):
@@ -76,9 +76,9 @@ class Template_Loc_Tech_Structure(models.Model):
         db_table = "template_locs_tech_structure"
         verbose_name_plural = "[Admin] Template Loc Tech Structure"
 
-    template = models.ForeignKey(template, on_delete=models.CASCADE)
-    template_loc = models.ForeignKey(template_loc_structure, on_delete=models.CASCADE)
-    template_tech = models.ForeignKey(template_tech_structure, on_delete=models.CASCADE)
+    template = models.ForeignKey(Template, on_delete=models.CASCADE)
+    template_loc = models.ForeignKey(Template_Loc_Structure, on_delete=models.CASCADE)
+    template_tech = models.ForeignKey(Template_Tech_Structure, on_delete=models.CASCADE)
 
     def __str__(self):
         return '%s' % (self.pretty_name)
@@ -88,7 +88,7 @@ class Template_Parameter(models.Model):
         db_table = "template_parameter"
         verbose_name_plural = "[Admin] Template Parameter"
 
-    template_tech = models.ForeignKey(template_locs_tech_structure, on_delete=models.CASCADE)
+    template_tech = models.ForeignKey(Template_Loc_Tech_Structure, on_delete=models.CASCADE)
     parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE)
     equation = models.CharField(max_length=200)
 
