bc21d557b3ff28ea3ea71ed0eb4394a5bc393ff3
hfields <hanna.fields@nrel.gov>
2023-03-13 16:33:09 -0600

    3/13

--- calliope_app/template/admin.py
+++ calliope_app/template/admin.py
@@ -1,36 +1,36 @@
 from django.contrib import admin
-from template.models import Template, Template_Option_Param, Template_Loc_Structure, Template_Tech_Structure, Template_Loc_Tech_Structure, Template_Parameter
+from template.models import Template_Types, Template_Type_Variables, Template_Type_Locs, Template_Type_Techs, Template_Type_Loc_Techs, Template_Type_Parameters
 
 # Register your models here.
-class Template_Admin(admin.ModelAdmin):
+class Template_Types_Admin(admin.ModelAdmin):
     # Add a different filter name?
     #list_filter = ['model']
     list_display = ['id', 'name', 'pretty_name', 'description']
-class Template_Option_Param_Admin(admin.ModelAdmin):
+class Template_Type_Variables_Admin(admin.ModelAdmin):
     # Add a different filter name?
     #list_filter = ['model']
     list_display = ['id', 'template', 'name', 'units', 'default_value', 'description']
-class Template_Loc_Structure_Admin(admin.ModelAdmin):
+class Template_Type_Locs_Admin(admin.ModelAdmin):
     # Add a different filter name?
     #list_filter = ['model']
     list_display = ['id', 'template', 'name', 'latitude_offset', 'longitude_offset', 'primary_location']
-class Template_Tech_Structure_Admin(admin.ModelAdmin):
+class Template_Type_Techs_Admin(admin.ModelAdmin):
     # Add a different filter name?
     #list_filter = ['model']
     list_display = ['id', 'template', 'name', 'abstract_tech', 'carrier_in', 'carrier_out']
-class Template_Loc_Tech_Structure_Admin(admin.ModelAdmin):
+class Template_Type_Loc_Techs_Admin(admin.ModelAdmin):
     # Add a different filter name?
     #list_filter = ['model']
     #Do loc_techs need a name too?
     list_display = ['id', 'template', 'template_loc', 'template_tech']
-class Template_Parameter(admin.ModelAdmin):
+class Template_Type_Parameters_Admin(admin.ModelAdmin):
     # Add a different filter name?
     #list_filter = ['model']
     list_display = ['id', 'template_tech', 'parameter', 'equation']
 
-admin.site.register(Template, Template_Admin)
-#admin.site.register(Template_Option_Param, Template_Option_Param_Admin)
-#admin.site.register(Template_Loc_Structure, Template_Loc_Structure_Admin)
-#admin.site.register(Template_Tech_Structure, Template_Tech_Structure_Admin)
-#admin.site.register(Template_Loc_Tech_Structure, Template_Loc_Tech_Structure_Admin)
-#admin.site.register(Template_Parameter, Template_Parameter_Admin)
+admin.site.register(Template_Types, Template_Types_Admin)
+admin.site.register(Template_Type_Variables, Template_Type_Variables_Admin)
+admin.site.register(Template_Type_Locs, Template_Type_Locs_Admin)
+admin.site.register(Template_Type_Techs, Template_Type_Techs_Admin)
+admin.site.register(Template_Type_Loc_Techs, Template_Type_Loc_Techs_Admin)
+admin.site.register(Template_Type_Parameters, Template_Type_Parameters_Admin)
--- calliope_app/template/models.py
+++ calliope_app/template/models.py
@@ -3,10 +3,10 @@ from api.models.calliope import Abstract_Tech, Parameter
 from api.models.configuration import Location
 
 # Create your models here.
-class Template(models.Model):
+class Template_Types(models.Model):
     class Meta:
-        db_table = "template"
-        verbose_name_plural = "[Admin] Template"
+        db_table = "template_types"
+        verbose_name_plural = "[Admin] Template Types"
 
     name = models.CharField(max_length=200)
     pretty_name = models.CharField(max_length=200)
@@ -16,29 +16,29 @@ class Template(models.Model):
         return '%s' % (self.pretty_name)
 
 
-class Template_Option_Param(models.Model):
+class Template_Type_Variables(models.Model):
     class Meta:
-        db_table = "template_option_param"
-        verbose_name_plural = "[Admin] Template Option Param"
+        db_table = "template_type_variables"
+        verbose_name_plural = "[Admin] Template Type Variables"
 
     name = models.CharField(max_length=200)
     units = models.CharField(max_length=200)
     default_value = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    template = models.ForeignKey(Template, on_delete=models.CASCADE)
+    template = models.ForeignKey(Template_Types, on_delete=models.CASCADE)
 
     def __str__(self):
         return '%s' % (self.pretty_name)
 
-class Template_Loc_Structure(models.Model):
+class Template_Type_Locs(models.Model):
     class Meta:
-        db_table = "template_loc_structure"
-        verbose_name_plural = "[Admin] Template Loc Structure"
+        db_table = "template_type_locs"
+        verbose_name_plural = "[Admin] Template Type Locs"
 
     name = models.CharField(max_length=200)
     latitude_offset = models.FloatField()
     longitude_offset = models.FloatField()
-    template = models.ForeignKey(Template, on_delete=models.CASCADE)
+    template = models.ForeignKey(Template_Types, on_delete=models.CASCADE)
     primary_location = models.ForeignKey(Location,
                                 on_delete=models.CASCADE,
                                 related_name="primary_location",
@@ -47,10 +47,10 @@ class Template_Loc_Structure(models.Model):
     def __str__(self):
         return '%s' % (self.pretty_name)
 
-class Template_Tech_Structure(models.Model):
+class Template_Type_Techs(models.Model):
     class Meta:
-        db_table = "template_tech_structure"
-        verbose_name_plural = "[Admin] Template Tech Structure"
+        db_table = "template_type_techs"
+        verbose_name_plural = "[Admin] Template Type Techs"
 
     name = models.CharField(max_length=200)
     abstract_tech = models.ForeignKey(Abstract_Tech, on_delete=models.CASCADE)
@@ -58,7 +58,7 @@ class Template_Tech_Structure(models.Model):
     carrier_in = models.CharField(max_length=200)
     carrier_out = models.CharField(max_length=200)
     #color = models.CharField(max_length=200) looks like technologies is doing something fancy with this
-    template = models.ForeignKey(Template, on_delete=models.CASCADE)
+    template = models.ForeignKey(Template_Types, on_delete=models.CASCADE)
 
     # @property
     # def carrier_in(self):
@@ -71,24 +71,24 @@ class Template_Tech_Structure(models.Model):
     def __str__(self):
         return '%s' % (self.pretty_name)
 
-class Template_Loc_Tech_Structure(models.Model):
+class Template_Type_Loc_Techs(models.Model):
     class Meta:
-        db_table = "template_locs_tech_structure"
-        verbose_name_plural = "[Admin] Template Loc Tech Structure"
+        db_table = "template_Type_locs_techs"
+        verbose_name_plural = "[Admin] Template Type Loc Techs"
 
-    template = models.ForeignKey(Template, on_delete=models.CASCADE)
-    template_loc = models.ForeignKey(Template_Loc_Structure, on_delete=models.CASCADE)
-    template_tech = models.ForeignKey(Template_Tech_Structure, on_delete=models.CASCADE)
+    template = models.ForeignKey(Template_Types, on_delete=models.CASCADE)
+    template_loc = models.ForeignKey(Template_Type_Locs, on_delete=models.CASCADE)
+    template_tech = models.ForeignKey(Template_Type_Techs, on_delete=models.CASCADE)
 
     def __str__(self):
         return '%s' % (self.pretty_name)
 
-class Template_Parameter(models.Model):
+class Template_Type_Parameters(models.Model):
     class Meta:
-        db_table = "template_parameter"
-        verbose_name_plural = "[Admin] Template Parameter"
+        db_table = "template_Type_parameters"
+        verbose_name_plural = "[Admin] Template Type Parameters"
 
-    template_tech = models.ForeignKey(Template_Loc_Tech_Structure, on_delete=models.CASCADE)
+    template_tech = models.ForeignKey(Template_Type_Loc_Techs, on_delete=models.CASCADE)
     parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE)
     equation = models.CharField(max_length=200)
 
