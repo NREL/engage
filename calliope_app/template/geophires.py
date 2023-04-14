08416b3725336924279724aaa30cb04d170e4618
hfields <hanna.fields@nrel.gov>
2023-04-14 10:51:41 -0600

    Add check geophires status endpoint

--- calliope_app/template/geophires.py
+++ calliope_app/template/geophires.py
@@ -7,6 +7,33 @@ from api.utils import initialize_units, convert_units_no_pipe
 
 @login_required
 @csrf_protect
+def request_complete(request):
+    """
+    Parameters:
+    external_api_id: required
+
+    Returns (json): Action Confirmation
+
+    Example:
+    POST: /geophires/
+    """
+    location_id = request.POST["location"]
+    formData = json.loads(request.POST["form_data"])
+    print(formData)
+    done = False #celeryTasksComplete() check _ meta table 
+    if done:
+        #readResults()
+        payload = {"complete": True,
+                }
+
+        return HttpResponse(json.dumps(payload), content_type="application/json")
+    else:
+        payload = {"complete": False,
+                }
+        return HttpResponse(json.dumps(payload), content_type="application/json")
+
+@login_required
+@csrf_protect
 def request_geophires(request):
     """
     Parameters:
@@ -32,7 +59,10 @@ def request_geophires(request):
     location_id = request.POST["location"]
     formData = json.loads(request.POST["form_data"])
     print(formData)
-
+    #seralize data
+    #check if row exists
+        #if it does return results meta record id and readResults()
+    #if it doesn't exist create one and start celery process
     if None in (location_id, formData["reservoir_heat_capacity"], formData["reservoir_density"], formData["reservoir_thermal_conductivity"], formData["gradient"],
         formData["min_temperature"], formData["max_temperature"], formData["min_reservoir_depth"], formData["max_reservoir_depth"], formData["min_production_wells"],
         formData["max_production_wells"], formData["min_injection_wells"], formData["max_injection_wells"]):
