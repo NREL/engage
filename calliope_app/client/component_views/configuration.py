591fcdb1997226cf0efdbe97dc4eec7fef6c9a26
Morris, James <James.Morris@nrel.gov>
2025-07-28 00:01:36 -0600

    Fixing unicode display issue
    We are now escaping all input on saving data. This was causing errors with django templating which auto-escapes, meaning that unicode characters wouldn't show up in the UI. Adding safe tags to the templating for all inputs that are escaped on save rectifies the issue

--- calliope_app/client/component_views/configuration.py
+++ calliope_app/client/component_views/configuration.py
@@ -247,6 +247,7 @@ def all_tech_params(request):
 
     timeseries = Timeseries_Meta.objects.filter(model=model, failure=False,
                                                 is_uploading=False)
+
     # Technology Definition
     context = {"technology": technology,
                "essentials": essentials,
@@ -456,9 +457,6 @@ def all_loc_tech_params(request):
                 'value': param["value"],
                 'placeholder': param["placeholder"]
             }]
-    
-    print(param_dict.keys())
-    print(param_dict['174NoneNone'])
 
     units_in_ids= ParamsManager.get_tagged_params('units_in')
     units_out_ids= ParamsManager.get_tagged_params('units_out')
